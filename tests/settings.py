b1d24c0c1a0f02a6d472ab6b117a762509b62034
joki <jonas.kiefer@live.com>
2023-05-12 10:58:18 +0200

    Update dev.txt, settings.py, and tox.ini

--- tests/settings.py
+++ tests/settings.py
@@ -3,9 +3,10 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 INSTALLED_APPS = [
-    "tests"
+    "tests",
 ]
 
-DATABASES = { "default": {'ENGINE': 'django.db.backends.sqlite3',  'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),}}
+DATABASES = {"default": {'ENGINE': 'django.db.backends.sqlite3',
+                         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'), }}
 
 DJANGO_TEST_ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
